274edc663693e66204c68290981adbb0a3c54823
Harsha <khurdhulaharshavardhan@gmail.com>
2023-02-14 13:53:58 -0500

    Creates a class, LanguageModels().

---
+++ HarshaVardhanKhurdulaHW3.py
@@ -0,0 +1,8 @@
+
+
+class LanguageModels():
+    """
+    This class aims to implement the two methods, that are required for the task.
+    1) train_LM(path_to_train_file: <str>): that trains a Language Model using a Bi gram, for specific file.
+    2) test_LM(path_to_test_file: <str>, LM_model: <object>): that tests the previously trained model. 
+    """
